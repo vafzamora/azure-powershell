31aee46f14523a01d8423fd595e7c8ac226cca69
Victor Zamora <vizamora@microsoft.com>
2024-02-19 10:10:52 -0300

    Add self-signed certificate generation and import to Key Vault

--- aks scripts/key-vault-cert.ps1
+++ aks scripts/key-vault-cert.ps1
@@ -12,6 +12,16 @@ param(
 $certName='aks-ingress-cert'
 $namespace='istio-ingress'
 
+# Generate self-signed certificate using openssl on Linux or Windows Subsystem for Linux (WSL) 
+#
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#     -out aks-ingress-tls.crt \
+#     -keyout aks-ingress-tls.key \
+#     -subj "/CN=demo.azure.com/O=aks-ingress-tls"
+
+# openssl pkcs12 -export -in aks-ingress-tls.crt -inkey aks-ingress-tls.key  -out $CERT_NAME.pfx
+# skip Password prompt
+
 # Import certificate into Key Vault using azure cli
 az keyvault certificate import --vault-name $KeyVaultName --name $certName --file "../$certName.pfx"
 
